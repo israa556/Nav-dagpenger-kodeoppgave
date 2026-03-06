7a364d50c14736045ebc3a6871e9be1228e309c4
israa556 <issul9239@oslomet.no>
2026-03-06 20:07:54 +0100

    La til en enkel Resultat-klasse

---
+++ src/main/java/no/nav/dagpenger/Resultat.java
@@ -0,0 +1,35 @@
+
+// Representerer resultatet av en dagpengerberegning som
+// senere kan behandles av saksbehandler
+public class Resultat {
+
+    private final double dagsats;
+    private final String type;
+    private String status;
+
+    public Resultat(double dagsats, String type){
+        this.dagsats = dagsats;
+        this.type = type;
+        this.status = "UBEHANDLET";
+    }
+
+    public double hentDagsats(){
+        return dagsats;
+    }
+
+    public String hentType(){
+        return type;
+    }
+
+    public String hentStatus(){
+        return status;
+    }
+
+    public void godkjenn(){
+        this.status = "GODKJENT";
+    }
+
+    public void avslå(){
+        this.status = "AVSLÅTT";
+    }
+}
