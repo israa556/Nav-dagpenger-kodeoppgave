931f8987b7d8b4160962fb90627dc58113e0c0b2
israa556 <issul9239@oslomet.no>
2026-03-07 23:52:42 +0100

    Ble ferdig med Saksbehandler-klassen.

--- src/main/java/no/nav/dagpenger/Saksbehandler.java
+++ src/main/java/no/nav/dagpenger/Saksbehandler.java
@@ -9,11 +9,24 @@ public class Saksbehandler {
     }
 
     public List<Resultat> hentUbehandledeResultater(List<Resultat> resultater){
+        List<Resultat> relevante = new ArrayList<>();
+
+        for(Resultat resultat : resultater){
+            boolean riktigSpesialisering = resultat.hentType().equals(spesialisering);
+            boolean erUbehandlet = resultat.hentStatus().equals("UBEHANDLET");
+
+            if(riktigSpesialisering && erUbehandlet){
+                relevante.add(resultat);
+            }
+        }
+        return relevante;
     }
+
 
     public void godkjenn(Resultat resultat) {
         resultat.godkjenn();
     }
+
 
     public void avslå(Resultat resultat){
         resultat.avslå();
